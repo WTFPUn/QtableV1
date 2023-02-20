848c95e7754b83337aae514d4912bf6d6e5571e6
grill <kanawatgrill2002@gmail.com>
2023-02-20 22:10:12 +0700

    มั่ว

--- scrpits/control_node.py
+++ scrpits/control_node.py
@@ -192,6 +192,7 @@ class ControlNode(Node):
                 ( lidar, angles ) = lidarScan(msgScan)
                 # ( state_ind, x1, x2, x3 , x4 , x5, x6, x7 ) = scanDiscretization(self.state_space, lidar)
                 ( state_ind, x1, x2, x3 , x4 , x5, x6, x7, x8, x9, x10 ) = scanDiscretization(self.state_space, lidar, (X_GOAL, Y_GOAL), (x, y), self.prev_position, self.MAX_RADIUS, GOAL_RADIUS)
+                # ( state_ind, x1, x2, x3 , x4 , x5, x6, x7, x8, x9 ) = scanDiscretization(self.state_space, lidar, (X_GOAL, Y_GOAL), (x, y), self.prev_position, self.MAX_RADIUS, GOAL_RADIUS)
                 
     
                 # Check for objects nearby
