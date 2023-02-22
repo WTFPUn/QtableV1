da40fb940097cb236771ba8c096eae72fece5e4f
Konthee <konthee1995@hotnmail.com>
2023-02-22 23:19:28 +0700

    push new algorithm
    Co-authored-by: Punnawat Namwongsa <WTFPUn@users.noreply.github.com>
    Co-authored-by: grill <gri11@users.noreply.github.com>

--- scrpits/control_node.py
+++ scrpits/control_node.py
@@ -6,7 +6,7 @@ import os
 from rclpy.node import Node
 import rclpy
 from std_msgs.msg import String
-
+import math
 from geometry_msgs.msg import Twist
 
 import sys
@@ -14,6 +14,8 @@ cwd = os.getcwd()
 print(cwd)
 cwd = os.path.dirname(cwd)
 print(cwd)
+
+cwd= 'Qtable/QtableV1'
 
 # TODO: Change to proper PATH
 DATA_PATH = os.path.join(cwd, 'Data')
@@ -37,12 +39,13 @@ REAL_ROBOT = True
 MIN_TIME_BETWEEN_ACTIONS = 0.0
 
 # Initial and goal positions
-INIT_POSITIONS_X = [ -0.7, -0.7, -0.5, -1, -2]
-INIT_POSITIONS_Y = [ -0.7, 0.7, 1, -2, 1]
-INIT_POSITIONS_THETA = [ 45, -45, -120, -90, 150]
-GOAL_POSITIONS_X = [ 2.0, 2.0, 0.5, 1, 2]
-GOAL_POSITIONS_Y = [ 1.0, -1.0, -1.9, 2, -1,]
-GOAL_POSITIONS_THETA = [ 25.0, -40.0, -40, 60, -30,]
+INIT_POSITIONS_X = [ 0] 
+INIT_POSITIONS_Y = [ 0]
+INIT_POSITIONS_THETA = [0]
+GOAL_POSITIONS_X = [ 3]
+GOAL_POSITIONS_Y = [ -2]
+GOAL_POSITIONS_THETA = [ 90]
+GOAL_RADIUS = .1
 
 PATH_IND = 4
 
@@ -51,8 +54,8 @@ if REAL_ROBOT:
     X_INIT = 0.0
     Y_INIT = 0.0
     THETA_INIT = 0.0
-    X_GOAL = 1.7
-    Y_GOAL = 1.1
+    X_GOAL = 3
+    Y_GOAL = -2
     THETA_GOAL = 90
 else:
     RANDOM_INIT_POS = False
@@ -93,8 +96,11 @@ class ControlNode(Node):
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
         self.t_step = self.get_clock().now()
         self.robot_in_pos = False
+        self.prev_position = (X_INIT, Y_INIT)
         self.count = 0
         self.Angle_det = 24
+        self.queuePos = []
+    
 
     def wait_for_message(
         node,
@@ -143,11 +149,9 @@ class ControlNode(Node):
         _, odomMsg = self.wait_for_message('/odom', Odometry)
         step_time = (self.get_clock().now() - self.t_step).nanoseconds / 1e9
 
-        self.prev_position = (999, 999)
-
         if step_time > MIN_TIME_BETWEEN_ACTIONS:
             self.t_step = self.get_clock().now()
-
+            status = ""
             if not self.robot_in_pos:
                 robotStop(self.velPub)
                 if REAL_ROBOT:
@@ -196,19 +200,23 @@ class ControlNode(Node):
                 length_lidar = len(lidar) 
                 ratio = length_lidar / 360 
             
-                ( state_ind, x1, x2, x3 , x4 , x5, x6, x7 ) = scanDiscretization(self.state_space, lidar, (X_GOAL, Y_GOAL), (x, y), self.prev_position, self.MAX_RADIUS, GOAL_RADIUS)
+                ( state_ind, x1, x2, x3 , x4 , x5, x6, x7 ) = scanDiscretization(self.state_space, lidar, (X_GOAL, Y_GOAL), (x, y), self.prev_position, GOAL_RADIUS)
                 
                 # get lidar value from x1 and x5 Zone
                 lidar_x1 = min(lidar[round(ratio*(90- self.Angle_det/2)): round(ratio*(90+ self.Angle_det/2+1))])
                 lidar_x5 = min(lidar[round(ratio*(270- self.Angle_det/2)): round(ratio*(270+ self.Angle_det/2+1))])
-                
 
                 # Check for objects nearby
                 crash = checkCrash(lidar)
                 object_nearby = checkObjectNearby(lidar)
                 goal_near = checkGoalNear(x, y, X_GOAL, Y_GOAL)
                 enable_feedback_control = True
-
+                posIsSame = False
+                self.queuePos.append((round(x, 3),round(y, 3)))
+                print(self.queuePos)
+                if len(self.queuePos) == 5:
+                    posIsSame = all(cond == self.queuePos[0] for cond in self.queuePos)
+                    self.queuePos.pop(0)
                 # Stop the simulation
                 if crash:
                     # robotStop(self.velPub)
@@ -219,6 +227,14 @@ class ControlNode(Node):
                         robotCW(self.velPub)
                     text = text + ' ==> Crash! backward'
                     status = 'Crash! backward'
+                
+                # U turn algorithm
+                # if last n position is the same coordinate.
+                if posIsSame:
+                    velMsg = createVelMsg(0.0, -2*math.pi)
+                    self.velPub.publish(velMsg)
+                    text = text + ' ==> U turn algorithm'
+                    posIsSame = False
 
                 # Feedback control algorithm
                 elif enable_feedback_control and ( not object_nearby and goal_near ):
@@ -230,6 +246,7 @@ class ControlNode(Node):
                 # go go powerranger algorithm
                 elif x1 == x5 and x3 == 2  :
                     robotGoSuperForward(self.velPub)
+                    text = text + ' ==> go go powerranger algorithm '
                 
                 # Q-learning algorithm
                 else:
@@ -241,7 +258,10 @@ class ControlNode(Node):
                     if not status == 'robotDoAction => OK':
                         print('\r\n', status, '\r\n')
                     text = text + ' ==> Q-learning algorithm'
-
+                
+                # set prev_position
+                self.prev_position = ( x , y )
+                
                 text = text + '\r\nx :       %.2f -> %.2f [m]' % (x, X_GOAL)
                 text = text + '\r\ny :       %.2f -> %.2f [m]' % (y, Y_GOAL)
                 text = text + '\r\ntheta :   %.2f -> %.2f [degrees]' % (degrees(theta), THETA_GOAL)
