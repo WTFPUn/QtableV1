4cc6962c63c23bb26e7b2489bf67246a03165a90
grill <kanawatgrill2002@gmail.com>
2023-02-24 01:22:24 +0700

    fix bug

--- scrpits/control_node.py
+++ scrpits/control_node.py
@@ -275,7 +275,7 @@ class ControlNode(Node):
                     posIsSame = False
                 
                 # opportunity algorithm  if robot not in goal zone(2m) do this
-                elif ( np.min(lidar) > WANGWANG) and np.linalg.norm((x,y) - (X_GOAL, Y_GOAL)) > 2:
+                elif ( np.min(lidar) > WANGWANG) and np.linalg.norm(np.array([x,y]) - np.array([X_GOAL, Y_GOAL])) > 2:
                         max = np.argmax([lidar_x1, lidar_x2, lidar_x3, lidar_x4, lidar_x5])
                         Angle = [-math.pi, -math.pi/4, "F", math.pi/4, math.pi]
                         if max == 2:
